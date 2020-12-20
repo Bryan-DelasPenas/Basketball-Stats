17ccb5f1b2974fd5496d19fbb7aae61d3225b52d
unknown <bryan.delaspenas405@gmail.com>
2020-12-20 16:17:37 -0500

    Created Player_Stats.py

--- Python Scrapers/Team_Stats_Scraper.py
+++ Python Scrapers/Team_Stats_Scraper.py
@@ -10,11 +10,11 @@
 # Format: 
 # Season Year, NBA TEAM NAME, Games played, Minutes played, Field goals, Field goals attempt, Field gold percentage,  
 # 3 point field goals, 3 points attempt, 3 point pecentage, 2 points field goals, 2 point field goal attempt, 2 point percentage 
-#  
+# TODO: Work on scraping 
 
 from bs4 import BeautifulSoup 
 
-def get_team_stats(): 
+def get_team_stats(season): 
     
 
 
