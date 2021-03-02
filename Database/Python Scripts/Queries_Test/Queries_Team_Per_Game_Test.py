b3f16d5f1ab5b96738512ab11731e887e32ef2b2
Bryan-DelasPenas <bryan.delaspenas405@gmail.com>
2021-03-02 14:39:03 -0500

    Completed Unit Testing for Team_Per_Game

--- Database/Python Scripts/Queries_Test/Queries_Team_Per_Game_Test.py
+++ Database/Python Scripts/Queries_Test/Queries_Team_Per_Game_Test.py
@@ -180,5 +180,160 @@ class TestQueryTeamPerGame(unittest.TestCase):
    
         pd.testing.assert_frame_equal(df_result, df_expected)
     
+    def test_create_query_all_team_per_game_both_sid(self):
+        # Create path to csv file    
+        path = os.path.join(pathlib.Path().absolute(), 'Database', 'Python Scripts', 'Queries_Test', 'Expected_Data', 'Query_All_Team_Per_Game_Both_2020.csv')
+        df_expected = pd.read_csv(path)
+        
+        df_expected = df_expected.astype({'Field_Goals_Made' : float, 'Field_Goals_Attempted' : float, 'Field_Goals_Percentage' : float, 'Three_Points_Made' : float, 
+        'Three_Points_Attempted' : float, 'Three_Points_Percentage' : float, 'Two_Points_Made' : float, 'Two_Points_Attempted' : float, 'Two_Points_Percentage' : float, 
+        'Free_Throws_Made' : float, 'Free_Throws_Attempted' : float, 'Free_Throws_Percentage' : float, 'Offensive_Rebound' : float, 'Defensive_Rebound' : float, 'True_Rebounds' : float,
+        'Assists' : float, 'Steals' : float, 'Blocks' : float, 'Turn_Over' : float, 'Personal_Foul' : float, 'Points' : float})
+       
+        # Connect to sql database
+        engine = create_connection()
+        
+        # Test the connection of the database
+        conn = test_connection(engine)
+        trans = conn.begin()
+
+        # Using the year 2020
+        result = conn.execute(
+        """
+        CALL query_all_team_per_game_both_sid(%s)
+        """, [2020]
+        ).fetchall()
+        
+        df_result = pd.DataFrame(result, columns=['Season_ID', 'Team_ID', 'Team_ABV', 'Team_Name', 'Games_Played', 'Minutes_Played', 'Field_Goals_Made', 'Field_Goals_Attempted', 
+        'Field_Goals_Percentage', 'Three_Points_Made', 'Three_Points_Attempted', 'Three_Points_Percentage', 'Two_Points_Made', 'Two_Points_Attempted', 'Two_Points_Percentage', 
+        'Free_Throws_Made', 'Free_Throws_Attempted', 'Free_Throws_Percentage', 'Offensive_Rebound', 'Defensive_Rebound', 'True_Rebounds', 'Assists', 'Steals', 'Blocks', 'Turn_Over', 
+        'Personal_Foul', 'Points', 'Opponent'])
+        
+        pd.testing.assert_frame_equal(df_result, df_expected)
+    
+    def test_create_query_all_team_per_game_both_tid(self):
+        # Create path to csv file    
+        path = os.path.join(pathlib.Path().absolute(), 'Database', 'Python Scripts', 'Queries_Test', 'Expected_Data', 'Query_All_Team_Per_Game_Both_Boston_Celtics.csv')
+        df_expected = pd.read_csv(path)
+        
+        df_expected = df_expected.astype({'Field_Goals_Made' : float, 'Field_Goals_Attempted' : float, 'Field_Goals_Percentage' : float, 'Three_Points_Made' : float, 
+        'Three_Points_Attempted' : float, 'Three_Points_Percentage' : float, 'Two_Points_Made' : float, 'Two_Points_Attempted' : float, 'Two_Points_Percentage' : float, 
+        'Free_Throws_Made' : float, 'Free_Throws_Attempted' : float, 'Free_Throws_Percentage' : float, 'Offensive_Rebound' : float, 'Defensive_Rebound' : float, 'True_Rebounds' : float,
+        'Assists' : float, 'Steals' : float, 'Blocks' : float, 'Turn_Over' : float, 'Personal_Foul' : float, 'Points' : float})
+
+        # Connect to sql database
+        engine = create_connection()
+        
+        # Test the connection of the database
+        conn = test_connection(engine)
+        trans = conn.begin()
+
+        # Using the year 2020
+        result = conn.execute(
+        """
+        CALL query_all_team_per_game_both_tid(%s)
+        """, [2]
+        ).fetchall()
+     
+        df_result = pd.DataFrame(result, columns=['Season_ID', 'Team_ID', 'Team_ABV', 'Team_Name', 'Games_Played', 'Minutes_Played', 'Field_Goals_Made', 'Field_Goals_Attempted', 
+        'Field_Goals_Percentage', 'Three_Points_Made', 'Three_Points_Attempted', 'Three_Points_Percentage', 'Two_Points_Made', 'Two_Points_Attempted', 'Two_Points_Percentage', 
+        'Free_Throws_Made', 'Free_Throws_Attempted', 'Free_Throws_Percentage', 'Offensive_Rebound', 'Defensive_Rebound', 'True_Rebounds', 'Assists', 'Steals', 'Blocks', 'Turn_Over', 
+        'Personal_Foul', 'Points', 'Opponent'])
+   
+        pd.testing.assert_frame_equal(df_result, df_expected)
+    
+    def test_create_query_all_team_per_game_both_sid_tid(self):
+        # Create path to csv file    
+        path = os.path.join(pathlib.Path().absolute(), 'Database', 'Python Scripts', 'Queries_Test', 'Expected_Data', 'Query_All_Team_Per_Game_Both_2020_Boston_Celtics.csv')
+        df_expected = pd.read_csv(path)
+        
+        df_expected = df_expected.astype({'Field_Goals_Made' : float, 'Field_Goals_Attempted' : float, 'Field_Goals_Percentage' : float, 'Three_Points_Made' : float, 
+        'Three_Points_Attempted' : float, 'Three_Points_Percentage' : float, 'Two_Points_Made' : float, 'Two_Points_Attempted' : float, 'Two_Points_Percentage' : float, 
+        'Free_Throws_Made' : float, 'Free_Throws_Attempted' : float, 'Free_Throws_Percentage' : float, 'Offensive_Rebound' : float, 'Defensive_Rebound' : float, 'True_Rebounds' : float,
+        'Assists' : float, 'Steals' : float, 'Blocks' : float, 'Turn_Over' : float, 'Personal_Foul' : float, 'Points' : float})
+
+        # Connect to sql database
+        engine = create_connection()
+        
+        # Test the connection of the database
+        conn = test_connection(engine)
+        trans = conn.begin()
+
+        # Using the year 2020
+        result = conn.execute(
+        """
+        CALL query_all_team_per_game_both_sid_tid(%s, %s)
+        """, [2020, 2]
+        ).fetchall()
+     
+        df_result = pd.DataFrame(result, columns=['Season_ID', 'Team_ID', 'Team_ABV', 'Team_Name', 'Games_Played', 'Minutes_Played', 'Field_Goals_Made', 'Field_Goals_Attempted', 
+        'Field_Goals_Percentage', 'Three_Points_Made', 'Three_Points_Attempted', 'Three_Points_Percentage', 'Two_Points_Made', 'Two_Points_Attempted', 'Two_Points_Percentage', 
+        'Free_Throws_Made', 'Free_Throws_Attempted', 'Free_Throws_Percentage', 'Offensive_Rebound', 'Defensive_Rebound', 'True_Rebounds', 'Assists', 'Steals', 'Blocks', 'Turn_Over', 
+        'Personal_Foul', 'Points', 'Opponent'])
+   
+        pd.testing.assert_frame_equal(df_result, df_expected)
+    
+    def test_create_query_all_team_per_game_both_name(self):
+        # Create path to csv file    
+        path = os.path.join(pathlib.Path().absolute(), 'Database', 'Python Scripts', 'Queries_Test', 'Expected_Data', 'Query_All_Team_Per_Game_Both_Boston_Celtics.csv')
+        df_expected = pd.read_csv(path)
+        
+        df_expected = df_expected.astype({'Field_Goals_Made' : float, 'Field_Goals_Attempted' : float, 'Field_Goals_Percentage' : float, 'Three_Points_Made' : float, 
+        'Three_Points_Attempted' : float, 'Three_Points_Percentage' : float, 'Two_Points_Made' : float, 'Two_Points_Attempted' : float, 'Two_Points_Percentage' : float, 
+        'Free_Throws_Made' : float, 'Free_Throws_Attempted' : float, 'Free_Throws_Percentage' : float, 'Offensive_Rebound' : float, 'Defensive_Rebound' : float, 'True_Rebounds' : float,
+        'Assists' : float, 'Steals' : float, 'Blocks' : float, 'Turn_Over' : float, 'Personal_Foul' : float, 'Points' : float})
+       
+        # Connect to sql database
+        engine = create_connection()
+        
+        # Test the connection of the database
+        conn = test_connection(engine)
+        trans = conn.begin()
+
+        # Using the year 2020
+        result = conn.execute(
+        """
+        CALL query_all_team_per_game_both_name(%s)
+        """, "Boston Celtics"
+        ).fetchall()
+        
+        df_result = pd.DataFrame(result, columns=['Season_ID', 'Team_ID', 'Team_ABV', 'Team_Name', 'Games_Played', 'Minutes_Played', 'Field_Goals_Made', 'Field_Goals_Attempted', 
+        'Field_Goals_Percentage', 'Three_Points_Made', 'Three_Points_Attempted', 'Three_Points_Percentage', 'Two_Points_Made', 'Two_Points_Attempted', 'Two_Points_Percentage', 
+        'Free_Throws_Made', 'Free_Throws_Attempted', 'Free_Throws_Percentage', 'Offensive_Rebound', 'Defensive_Rebound', 'True_Rebounds', 'Assists', 'Steals', 'Blocks', 'Turn_Over', 
+        'Personal_Foul', 'Points', 'Opponent'])
+        
+        pd.testing.assert_frame_equal(df_result, df_expected)
+    
+    def test_create_query_all_team_per_game_both_ABV(self):
+        # Create path to csv file    
+        path = os.path.join(pathlib.Path().absolute(), 'Database', 'Python Scripts', 'Queries_Test', 'Expected_Data', 'Query_All_Team_Per_Game_Both_Boston_Celtics.csv')
+        df_expected = pd.read_csv(path)
+        
+        df_expected = df_expected.astype({'Field_Goals_Made' : float, 'Field_Goals_Attempted' : float, 'Field_Goals_Percentage' : float, 'Three_Points_Made' : float, 
+        'Three_Points_Attempted' : float, 'Three_Points_Percentage' : float, 'Two_Points_Made' : float, 'Two_Points_Attempted' : float, 'Two_Points_Percentage' : float, 
+        'Free_Throws_Made' : float, 'Free_Throws_Attempted' : float, 'Free_Throws_Percentage' : float, 'Offensive_Rebound' : float, 'Defensive_Rebound' : float, 'True_Rebounds' : float,
+        'Assists' : float, 'Steals' : float, 'Blocks' : float, 'Turn_Over' : float, 'Personal_Foul' : float, 'Points' : float})
+       
+        # Connect to sql database
+        engine = create_connection()
+        
+        # Test the connection of the database
+        conn = test_connection(engine)
+        trans = conn.begin()
+
+        # Using the year 2020
+        result = conn.execute(
+        """
+        CALL query_all_team_per_game_both_abv(%s)
+        """, "BOS"
+        ).fetchall()
+        
+        df_result = pd.DataFrame(result, columns=['Season_ID', 'Team_ID', 'Team_ABV', 'Team_Name', 'Games_Played', 'Minutes_Played', 'Field_Goals_Made', 'Field_Goals_Attempted', 
+        'Field_Goals_Percentage', 'Three_Points_Made', 'Three_Points_Attempted', 'Three_Points_Percentage', 'Two_Points_Made', 'Two_Points_Attempted', 'Two_Points_Percentage', 
+        'Free_Throws_Made', 'Free_Throws_Attempted', 'Free_Throws_Percentage', 'Offensive_Rebound', 'Defensive_Rebound', 'True_Rebounds', 'Assists', 'Steals', 'Blocks', 'Turn_Over', 
+        'Personal_Foul', 'Points', 'Opponent'])
+        
+        pd.testing.assert_frame_equal(df_result, df_expected)
+
 if __name__ == '__main__':
     unittest.main()
