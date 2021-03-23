029b2f2778e187f3347472f46038f85c775b3e75
Bryan-DelasPenas <bryan.delaspenas405@gmail.com>
2021-03-23 18:38:21 -0400

    Added csv files for every team and there players

--- Database/Python Scripts/Call_Queries/Constants.py
+++ Database/Python Scripts/Call_Queries/Constants.py
@@ -17,6 +17,10 @@ UPDATE_NAME = {
 }
 '''
 
+PLAYED_FOR_TEAM = {
+    1: []
+}
+
 def create_team_id_dict():
     
     team_id_dict = {}
@@ -36,44 +40,4 @@ def create_team_id_dict():
         count+= 1
     return team_id_dict
 
-def create_team_name_dict():
-    
-    team_name_dict = {}
-    path = os.path.join(pathlib.Path().absolute(), "Web_Scrapers",'Output', 'Season', 'Team_Names')
-    team_files = os.listdir(path)
-    count = 1980
-    for team in team_files:
-        
-        # Convert file name into dataframes
-        df = pd.read_csv(path + "\\" + team)
-      
-        # Create a list
-        team_name_list = df['Team'].tolist()
-
-        # Add it to the dict and increase count by 1
-        team_name_dict[count] = team_name_list
-        count+= 1
-    return team_name_dict
-
-def create_team_abv_dict():
-    
-    team_abv_dict = {}
-    path = os.path.join(pathlib.Path().absolute(), "Web_Scrapers",'Output', 'Season', 'Team_Names')
-    team_files = os.listdir(path)
-    count = 1980
-    for team in team_files:
-        
-        # Convert file name into dataframes
-        df = pd.read_csv(path + "\\" + team)
-      
-        # Create a list
-        team_abv_list = df['Team ABV'].tolist()
-
-        # Add it to the dict and increase count by 1
-        team_abv_dict[count] = team_abv_list
-        count+= 1
-    return team_abv_dict
-
 VALID_SID_TEAM_IDS = create_team_id_dict()
-VALID_SID_TEAM_NAMES = create_team_name_dict()
-VALID_SID_TEAM_ABVS = create_team_abv_dict()
