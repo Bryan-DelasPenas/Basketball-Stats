50807b369e22ca4b4188bbff686d242aa180b3b0
Bryan-DelasPenas <bryan.delaspenas405@gmail.com>
2021-03-30 22:29:46 -0400

    Completed Unit Testing for Confernce_Standings

--- Database/Python Scripts/Queries_Test/Queries_Conference_Standings_Test.py
+++ Database/Python Scripts/Queries_Test/Queries_Conference_Standings_Test.py
@@ -6,6 +6,7 @@ import os
 import unittest
 sys.path.append(str(pathlib.Path().absolute()) + '\\Database' +'\\Python Scripts')
 sys.path.append(str(pathlib.Path().absolute()) + '\\Database' +'\\Python Scripts' + '\\Queries')
+sys.path.append(str(pathlib.Path().absolute()) + '\\Database' +'\\Python Scripts' + '\\Call_Queries')
 
 import pyodbc
 import sqlalchemy as sal
@@ -13,6 +14,7 @@ from sqlalchemy import create_engine
 
 from Helper_DB import create_connection, test_connection, check_table
 from Query_Conference_Standings import create_cs_query, drop_cs_query
+from Call_Query_Conference_Standings import *
 
 '''
 Class that will Conference_Standings_Queries, Assuming that the data has been inserted correctly and the procedures are created
@@ -31,183 +33,60 @@ class TestQueryConferenceStandings(unittest.TestCase):
         path = os.path.join(pathlib.Path().absolute(), 'Database', 'Python Scripts', 'Queries_Test', 'Expected_Data', 'Query_All_CS_2020.csv')
         df_expected = pd.read_csv(path)
         
-        # Connect to sql database
-        engine = create_connection()
-        
-        # Test the connection of the database
-        conn = test_connection(engine)
-        trans = conn.begin()
-        
-        result = conn.execute(
-        """
-        CALL query_all_cs_sid(%s)
-        """, 2020
-        ).fetchall()
-       
-        df_result = pd.DataFrame(result, 
-        columns=['Season_ID', 'Team_ID', 'Team_ABV', 'Team_Name', 'Wins', 'Loses', 'Win_Lose_Percentage', 
-                'Games_Behind', 'Points_Per_Game', 'Opponents_Points_Per_Game', 'Simple_Rating_System', 'East_Or_West'])
-      
+        df_result = call_query_all_cs_sid(2020)
         pd.testing.assert_frame_equal(df_result, df_expected)
-    
+     
     def test_create_query_all_cs_tid(self):
         path = os.path.join(pathlib.Path().absolute(), 'Database', 'Python Scripts', 'Queries_Test', 'Expected_Data', 'Query_All_CS_Boston_Celtics.csv')
         df_expected = pd.read_csv(path)
         
-        # Connect to sql database
-        engine = create_connection()
-        
-        # Test the connection of the database
-        conn = test_connection(engine)
-        trans = conn.begin()
-        
-        result = conn.execute(
-        """
-        CALL query_all_cs_tid(%s)
-        """, 2
-        ).fetchall()
-       
-        df_result = pd.DataFrame(result, 
-        columns=['Season_ID', 'Team_ID', 'Team_ABV', 'Team_Name', 'Wins', 'Loses', 'Win_Lose_Percentage', 
-                'Games_Behind', 'Points_Per_Game', 'Opponents_Points_Per_Game', 'Simple_Rating_System', 'East_Or_West'])
-      
+        df_result = call_query_all_cs_tid(2)
         pd.testing.assert_frame_equal(df_result, df_expected)
   
     def test_create_query_all_cs_name(self):
         path = os.path.join(pathlib.Path().absolute(), 'Database', 'Python Scripts', 'Queries_Test', 'Expected_Data', 'Query_All_CS_Boston_Celtics.csv')
         df_expected = pd.read_csv(path)
         
-        # Connect to sql database
-        engine = create_connection()
         
-        # Test the connection of the database
-        conn = test_connection(engine)
-        trans = conn.begin()
-        
-        result = conn.execute(
-        """
-        CALL query_all_cs_name(%s)
-        """, "Boston Celtics"
-        ).fetchall()
-       
-        df_result = pd.DataFrame(result, 
-        columns=['Season_ID', 'Team_ID', 'Team_ABV', 'Team_Name', 'Wins', 'Loses', 'Win_Lose_Percentage', 
-                'Games_Behind', 'Points_Per_Game', 'Opponents_Points_Per_Game', 'Simple_Rating_System', 'East_Or_West'])
-      
+        df_result = call_query_all_cs_name('Boston Celtics')
         pd.testing.assert_frame_equal(df_result, df_expected)
 
     def test_create_query_all_cs_ABV(self):
         path = os.path.join(pathlib.Path().absolute(), 'Database', 'Python Scripts', 'Queries_Test', 'Expected_Data', 'Query_All_CS_Boston_Celtics.csv')
         df_expected = pd.read_csv(path)
         
-        # Connect to sql database
-        engine = create_connection()
-        
-        # Test the connection of the database
-        conn = test_connection(engine)
-        trans = conn.begin()
-        
-        result = conn.execute(
-        """
-        CALL query_all_cs_abv(%s)
-        """, "BOS"
-        ).fetchall()
-       
-        df_result = pd.DataFrame(result, 
-        columns=['Season_ID', 'Team_ID', 'Team_ABV', 'Team_Name', 'Wins', 'Loses', 'Win_Lose_Percentage', 
-                'Games_Behind', 'Points_Per_Game', 'Opponents_Points_Per_Game', 'Simple_Rating_System', 'East_Or_West'])
-      
+    
+        df_result = call_query_all_cs_ABV('BOS')
         pd.testing.assert_frame_equal(df_result, df_expected)
 
     def test_create_query_all_cs_win(self):
         path = os.path.join(pathlib.Path().absolute(), 'Database', 'Python Scripts', 'Queries_Test', 'Expected_Data', 'Query_All_CS_60wins.csv')
         df_expected = pd.read_csv(path)
         
-        # Connect to sql database
-        engine = create_connection()
-        
-        # Test the connection of the database
-        conn = test_connection(engine)
-        trans = conn.begin()
-        
-        result = conn.execute(
-        """
-        CALL query_all_cs_win(%s)
-        """, 60
-        ).fetchall()
-       
-        df_result = pd.DataFrame(result, 
-        columns=['Season_ID', 'Team_ID', 'Team_ABV', 'Team_Name', 'Wins', 'Loses', 'Win_Lose_Percentage', 
-                'Games_Behind', 'Points_Per_Game', 'Opponents_Points_Per_Game', 'Simple_Rating_System', 'East_Or_West'])
-      
+        df_result = call_query_all_cs_win(60)
         pd.testing.assert_frame_equal(df_result, df_expected)
 
     def test_create_query_all_cs_wl(self):
         path = os.path.join(pathlib.Path().absolute(), 'Database', 'Python Scripts', 'Queries_Test', 'Expected_Data', 'Query_All_CS_80_percent.csv')
         df_expected = pd.read_csv(path)
         
-        # Connect to sql database
-        engine = create_connection()
-        
-        # Test the connection of the database
-        conn = test_connection(engine)
-        trans = conn.begin()
-        
-        result = conn.execute(
-        """
-        CALL query_all_cs_wl(%s)
-        """, .8
-        ).fetchall()
-       
-        df_result = pd.DataFrame(result, 
-        columns=['Season_ID', 'Team_ID', 'Team_ABV', 'Team_Name', 'Wins', 'Loses', 'Win_Lose_Percentage', 
-                'Games_Behind', 'Points_Per_Game', 'Opponents_Points_Per_Game', 'Simple_Rating_System', 'East_Or_West'])
-      
+        df_result = call_query_all_cs_wl(.8)
         pd.testing.assert_frame_equal(df_result, df_expected)
 
     def test_create_query_all_cs_sid_ew(self):
         path = os.path.join(pathlib.Path().absolute(), 'Database', 'Python Scripts', 'Queries_Test', 'Expected_Data', 'Query_All_CS_2020_East.csv')
         df_expected = pd.read_csv(path)
         
-        # Connect to sql database
-        engine = create_connection()
-        
-        # Test the connection of the database
-        conn = test_connection(engine)
-        trans = conn.begin()
-        
-        result = conn.execute(
-        """
-        CALL query_all_cs_sid_ew(%s, %s)
-        """, [2020, 0]
-        ).fetchall()
-       
-        df_result = pd.DataFrame(result, 
-        columns=['Season_ID', 'Team_ID', 'Team_ABV', 'Team_Name', 'Wins', 'Loses', 'Win_Lose_Percentage', 
-                'Games_Behind', 'Points_Per_Game', 'Opponents_Points_Per_Game', 'Simple_Rating_System', 'East_Or_West'])
-      
+        df_result = call_query_all_cs_sid_ew(2020, 0)
         pd.testing.assert_frame_equal(df_result, df_expected)
 
     def test_create_query_cs_win(self):
         data = [[2020, 2, 'BOS', 'Boston Celtics', 48]]
         df_expected = pd.DataFrame(data, columns=['Season_ID', 'Team_ID', 'Team_ABV', 'Team_Name', 'Wins'])
-        # Connect to sql database
-        engine = create_connection()
         
-        # Test the connection of the database
-        conn = test_connection(engine)
-        trans = conn.begin()
-        
-        result = conn.execute(
-        """
-        CALL query_cs_win(%s, %s)
-        """, [2020, 2]
-        ).fetchall()
-    
-        df_result = pd.DataFrame(result, columns=['Season_ID', 'Team_ID', 'Team_ABV', 'Team_Name', 'Wins'])
-        
+        df_result = call_query_cs_win(2020, 2)
         pd.testing.assert_frame_equal(df_result, df_expected)
-    
+
 if __name__ == '__main__':
     unittest.main()
     
