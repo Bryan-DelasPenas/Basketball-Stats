a2b2a0c5e7be6aa31ea6b44e34569da9d118306a
Bryan-DelasPenas <bryan.delaspenas405@gmail.com>
2021-04-27 04:29:22 -0400

    1. Changed comments for file paths 2. Added function calls for ROY AND MVP voting in get_season_csv

--- Web_Scrapers/Run_Scraper/get_season_stats.py
+++ Web_Scrapers/Run_Scraper/get_season_stats.py
@@ -13,18 +13,15 @@ from helper import create_output_directory, create_years_dir
 Generates a CSV file of team names for a season 
 '''
 def csv_team_name(year):
-    # Check if the needed directory has been made  
-    directory_source = "Season"
-    directory_parent = "Team_Names"
-    
-    # Get the first file path
-    first_path = first_path = os.path.join(pathlib.Path().absolute(), "Web_Scrapers", "Output", directory_source , directory_parent)
+    # ... /src/Web_Scrapers/Output/Season/Team_Name
+    first_path = os.path.join(pathlib.Path().absolute(), "Web_Scrapers", "Output", "Season", "Team_Name")
     if(not os.path.isdir(first_path)):
         
-        # Create the directory with the final_path
+        # Create the directory with the first_path
         os.mkdir(first_path)
     else:
         pass
+    
     # Call the scraper
     df = get_team_name(year)
     
@@ -47,23 +44,18 @@ def csv_standings(year, format):
     if format not in valid_format:
         print("Error: Please look at WebScraper_UserManual for the right parameters")
         return None
-    
-    # Check if the proper directories has been made
-    directory_source = "Web_Scrapers\Output"
-    directory_grand_parent = "Season"
-    directory_parent = "Standings"
-
-    first_path =  os.path.join(pathlib.Path().absolute(), directory_source , directory_grand_parent)
-
+   
+    # ... src/Web_Scrapers\Output\Season
+    first_path =  os.path.join(pathlib.Path().absolute(), "Web_Scrapers\Output\Season")
     if(not os.path.isdir(first_path)):
-        # Create the directory with the final_path
+        # Create the directory with the first_path
         os.mkdir(first_path)
 
-    # Create the final path that has format name
-    second_path = os.path.join(first_path, directory_parent)
+    # ... src/Web_Scrapers\Output\Season\Standings
+    second_path = os.path.join(first_path, "Standings")
     if(not os.path.isdir(second_path)):
         
-        # Create the directory with the final_path
+        # Create the directory with the second_path
         os.mkdir(second_path)
   
     # If the format is standard get_standings will return two dataframes 
@@ -74,9 +66,11 @@ def csv_standings(year, format):
         west_output_path = os.path.join(second_path, "East")
         east_output_path = os.path.join(second_path, "West")
 
+        # ... src/Web_Scrapers\Output\Season\Standings\West
         if(not os.path.isdir(west_output_path)): 
             os.mkdir(west_output_path)
        
+        # ... src/Web_Scrapers\Output\Season\Standings\East
         if (not os.path.isdir(east_output_path)): 
             os.mkdir(east_output_path) 
     
@@ -150,6 +144,12 @@ def get_season_csv():
         # Standing Stats
         csv_standings(year, 'STANDARD')
 
+        # MVP voting
+        csv_award_voting(year, 'MVP')
+        
+        # ROY voting
+        csv_award_voting(year, 'ROY')
+        
 '''
 Main Function
 '''
